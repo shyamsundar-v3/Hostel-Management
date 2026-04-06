f2f50d92e1d091693c6dcbef806b56f0d6177c7b
Shyam Sundar <shyamsunder4689@gmail.com>
2026-04-07 02:18:10 +0530

    Refactor MongoDB connection configuration
    Simplified MongoDB connection options and improved error logging.

--- server/config/mongoDBConfig.js
+++ server/config/mongoDBConfig.js
@@ -2,16 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    console.log("MONGO_URI:", process.env.MONGO_URI);
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(error.message);
     process.exit(1);
   }
 };
